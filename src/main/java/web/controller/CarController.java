296fa76fceef451335116453af85109aa16af983
Talgat <accurate19@mail.ru>
2024-08-23 17:32:57 +0300

    New fixes

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -14,29 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 @Controller()
 public class CarController {
 
-//    @GetMapping("/cars")
-//    public String printTable(HttpServletRequest request) {
-//        int countOfCars = request.getParameterMap().size();
-//
-//        System.out.println("Количество будущих столбцов: " + countOfCars);
-//        return "cars";
-//    }
-
     public final CarDAO carDAO;
 
     @Autowired
     public CarController(CarDAO carDAO) {
         this.carDAO = carDAO;
     }
-
-
-    @GetMapping("/cars/{count}")
-    public String printTable(@PathVariable("count") Integer count, Model model) {
-        model.addAttribute("cars", carDAO.carTable(count));
-        return "cars";
-    }
-
-
 
     @GetMapping("/cars")
     public String printTable(@RequestParam(name = "count", required = false, defaultValue = "0") Integer count, ModelMap model) {
